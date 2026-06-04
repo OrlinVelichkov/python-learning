039dfbdf8ac04d890284982db958add3f7e0d607
Orlin Velichkov <orlin.velichkov87@gmail.com>
2026-06-04 23:52:46 +0300

    feat: add enumerate start patterns

---
+++ lists/list_enumerate_start_patterns.py
@@ -0,0 +1,17 @@
+#Active Coding — Session 2
+# Task 9
+books = ["Book A", "Book B", "Book C"]
+for i, book in enumerate(books, start=1):
+    print(f'{i} {book}')
+# Task 10
+clients = ["Ivan", "Maria", "Petar", "Georgi"]
+for i, client in enumerate(clients, start=1):
+    print(f'Client {i}: {client}')
+# Task 11
+orders = [3, 5, 2, 8]
+for i, sales in enumerate(orders, start=1):
+    print(f'Day {i}: {sales} orders')
+# Task 12
+my_top_dreams = ['Actor', 'Grosmaster', 'Fighter jet pilot' ]
+for i, dream in enumerate(my_top_dreams, start=1):
+    print(f'My top dream №{i}: {dream}')
