2c207b5dd7fb9af5f217fda6078ab6a8a3be1141
Orlin Velichkov <orlin.velichkov87@gmail.com>
2026-06-07 23:16:27 +0300

    feat: add zip integration challenges

---
+++ lists/list_any_all_fundamentals.py
@@ -0,0 +1,43 @@
+#start learning at 22:22
+
+print(any([False, False, True]))
+#Output True
+
+
+print(all([False, False, True]))
+#Output False
+
+# Active Coding — Session 1
+# Task 1
+results = [True, False, False]
+print(any(results))
+#Output True
+# Task 2
+results = [False, False, False]
+print(any(results))
+#Output False
+# Task 3
+results = [True, True, True]
+print(all(results))
+#Output True
+# Task 4
+results = [True, False, True]
+print(all(results))
+#Output False
+# Task 5
+buy = [1, 2, 3]
+print(any(buy))
+# Task 6
+paid = [0, 50, 50, 0]
+print(all(paid))
+# Task 7 — Mini Thinking
+print(any([True, True, True]))
+#Output True
+print(all([True, True, True]))
+#Output True
+# Task 8 — Mini Thinking
+print(any([False, False, False]))
+#Output False
+print(all([False, False, False]))
+#Output False
+
