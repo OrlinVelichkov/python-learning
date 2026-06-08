19ae77fb63303361da93640e27d4b2c65862a96f
Orlin Velichkov <orlin.velichkov87@gmail.com>
2026-06-08 23:43:09 +0300

    feat: add slicing patterns with functions

---
+++ lists/list_functions_slicing_patterns.py
@@ -0,0 +1,27 @@
+#08.06.2026 - 22:10h
+# Active Coding — Session 4
+# Task 19
+numbers = [10, 20, 30, 40]
+def get_reversed(items):
+    return items[::-1]
+print(get_reversed(numbers))
+# Task 20
+cities = ["Sofia", "Varna", "Plovdiv", "Burgas"]
+def get_first_two(items):
+    return items[:2]
+print(get_first_two(cities))
+# Task 21
+books = ["Nemo", "Atlantis", "Robin Hood", "Sherlock"]
+def get_last_two(items):
+    return items[-2:]
+print(get_last_two(books))
+# Task 22
+numbers = [10, 20, 30, 40, 50, 60]
+def get_even(items):
+    return items[::2]
+print(get_even(numbers))
+# Task 23
+total_calories = [120, 500, 249, 85]
+def last_two_taken(items):
+    return items[-2:]
+print(last_two_taken(total_calories))
